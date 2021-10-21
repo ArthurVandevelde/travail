bcb67f285b12c8a62c53d25b3f6db3eb4aea1bb7
ArthurVandevelde <91486150+ArthurVandevelde@users.noreply.github.com>
2021-10-21 12:27:00 +0200

    Add files via upload

---
+++ bonjour.py
@@ -0,0 +1,14 @@
+def triangle(c1,c2,c3):
+    if c1=c2:
+        if c1=c3:
+            print('c est un triangle isocèle')
+        elif c1>c2=c3:
+            print('c est un triangle isocèle')
+        elif c1=c3 :
+            print('c est un triangle isocèle')
+        elif c1=c2:
+            print('c est un triangle isocèle')
+    elif c1=c2=c3 :
+        print('c est un triangle équilatéral')
+    else:
+        print('c est un triangle scalène')
