9e6cab371bda69c64656bdad6fb8bb2f5f47ab9d
ArthurVandevelde <91486150+ArthurVandevelde@users.noreply.github.com>
2022-01-24 17:43:27 +0100

    Add files via upload

---
+++ i.py
@@ -0,0 +1,15 @@
+#exercice2
+##n=0
+##while n<10:
+##    print(n)
+##    n=n+2
+##    
+#Exercice3
+def decroissant_for(n):
+    for i in range(n,0,-1):
+        print(i, end='  ')
+        
+def decroissant_while(n):
+    while n >0:
+        print(n, end=' ')
+        n=n-1
