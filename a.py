a0eefc2f4eeabb1ede29de3ef772f88ecaccad50
ArthurVandevelde <91486150+ArthurVandevelde@users.noreply.github.com>
2022-02-03 12:53:08 +0100

    Add files via upload

---
+++ a.py
@@ -0,0 +1,18 @@
+#Exercice2
+def quatreOperations(a, b):
+    adition = a + b
+    soustraction = a - b
+    multiplication = a * b
+    division = a / b
+    return adition, soustraction, multiplication, division
+
+    
+    
+    
+##monTuple = quatreOperations(8,2)
+##
+##print(monTuple)
+##
+### On remarque bien que monTuple contient 4 éléments la 8+2, 8-2, 8*2 et 8/2
+
+quatreOperations(8,2)
