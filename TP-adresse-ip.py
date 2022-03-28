086875f8b5c21a21dc0ceb505580debf55042f6d
ArthurVandevelde <91486150+ArthurVandevelde@users.noreply.github.com>
2022-03-28 17:51:26 +0200

    Update TP-adresse-ip.py

--- TP-adresse-ip.py
+++ TP-adresse-ip.py
@@ -1,8 +1,9 @@
 def sous_reseau (ip,masque):
+    c = []
     a = ip.split(".")
     b=masque.split(".")
     for i in range(4):
-        c= str(a[i]) & str(b[i])
+        c= c + a[i] & b[i]
     return c
 
 sous_reseau(192.168.16.8,255.255.0.0)
